980503837a94422c0d061b2873e9a8d751731814
apoorvachikara <chikara02apoorva@gmail.com>
2019-10-02 12:21:49 +0530

    added cors for backend

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const Post = require('./models/postModel');
 
@@ -10,24 +11,26 @@ const userRoutes = require('./routes/user');
 
 // mongodb+srv://post-application-DB:9870228146@meanstackapp-x1a5a.mongodb.net/node-angular?retryWrites=true&w=majority
 // 'mongodb://localhost/meanstackapp'
-mongoose.connect('mongodb://localhost/meanstackapp')
+mongoose.connect(' mongodb+srv://post-application-DB:9870228146@meanstackapp-x1a5a.mongodb.net/node-angular?retryWrites=true&w=majority')
   .then(() => {
     console.log("Connected to DB successfully");
   })
   .catch((error) => {
       throw error;
-  })
+  });
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 app.use("/images", express.static(path.join("backend/images")));
 
-app.use((req, res, next) => {
-  res.setHeader("Acess-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*")
-  next();
-})
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.setHeader("Acess-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "*")
+//   next();
+// })
 
 //
 app.use("/api/posts", postRoutes);
